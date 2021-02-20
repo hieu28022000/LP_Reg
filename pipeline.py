7b00b6cb5d60d4f8c929001c47bba8915ac69cc9
khangnh <nskhang1@gmail.com>
2021-02-20 17:25:24 +0700

    update baseline

--- pipeline.py
+++ pipeline.py
@@ -44,7 +44,23 @@ def text_detect_CRAFT(image_path, craft_config, net_craft, sortbb=True, visual_i
     return img, bboxes, polys, score_text
 
 if __name__ == '__main__':
-    # text = text_recog(cfg, 'data/illusion.png')
-    img, bboxes, polys, score_text = text_detect_CRAFT('data/test.jpg', CRAFT_CONFIG, NET_CRAFT)
-    cv2.imshow("image", img)
-    cv2.waitKey(0)
+    # img = cv2.imread('data/Reg_data/00-A2_170.05.jpg')
+
+    # predict region of bounding box
+    # # img, bboxes, polys, score_text = text_detect_CRAFT('data/test.jpg', CRAFT_CONFIG, NET_CRAFT)
+
+    # visualize bounding box
+    # print ('bboxes--------',bboxes)
+    # print ('score text---------', score_text)
+    # print ('polys-------', polys)
+    # count = 1
+    # for i, line in enumerate(bboxes):
+    #     print ('iiiiiiiiiiiii: ', line[0])
+    #     cv2.rectangle(img, (line[0][0], line[0][1]), (line[2][0], line[2][1]), (0,255,0), 1)
+    #     cv2.putText(img, str(count), (line[0][0], line[0][1]), cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(255,0,0), thickness=1)
+    #     count += 1    
+    # cv2.imwrite('img_re.jpg', img)
+
+    # regconition a image
+    text = text_recog (cfg, 'data/170.05.png')
+    print ('text: ', text)
